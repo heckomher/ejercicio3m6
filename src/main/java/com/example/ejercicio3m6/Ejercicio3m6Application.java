06744319634d3462d399095cef7cd3fbceb72c28
JoaqinDLH <134020388+JoaqinDLH@users.noreply.github.com>
2023-08-02 22:22:22 -0400

    Changes by heckomher

--- src/main/java/com/example/ejercicio3m6/Ejercicio3m6Application.java
+++ src/main/java/com/example/ejercicio3m6/Ejercicio3m6Application.java
@@ -3,6 +3,7 @@ package com.example.ejercicio3m6;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Ejercicio3m6Application {
 
