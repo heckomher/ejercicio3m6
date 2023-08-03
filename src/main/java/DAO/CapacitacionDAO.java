24809f5c1c52e576545215dacdc3dad41cc2a85d
JoaqinDLH <134020388+JoaqinDLH@users.noreply.github.com>
2023-08-02 23:05:36 -0400

    pusheo 02.08 - 22.21

--- src/main/java/DAO/CapacitacionDAO.java
+++ src/main/java/DAO/CapacitacionDAO.java
@@ -1,4 +1,22 @@
 package DAO;
+import java.util.ArrayList;
+import java.util.List;
 
-public class CapacitacionDAO {
+public class CapacitacionDao implements ICapacitacionDao {
+
+    // Lista para almacenar las capacitaciones (simulación de una base de datos)
+    private List<Capacitacion> capacitaciones = new ArrayList<>();
+
+    @Override
+    public List<Capacitacion> obtenerCapacitaciones() {
+        // Simulamos la obtención de capacitaciones de una base de datos o algún otro sistema de almacenamiento
+        return capacitaciones;
+    }
+
+    @Override
+    public boolean crearCapacitacion(Capacitacion capacitacion) {
+        // Simulamos la creación de una capacitación en una base de datos o algún otro sistema de almacenamiento
+        // Agregamos la capacitación a la lista (simulación)
+        return capacitaciones.add(capacitacion);
+    }
 }
